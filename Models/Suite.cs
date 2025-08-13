f6ed1861130bb870760e4109021976fb0417eb28
Mayara Marques <mayaracxmarques@gmail.com>
2025-08-13 06:25:51 -0300

    Melhorando o projeto

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -1,18 +1,27 @@
 namespace DesafioProjetoHospedagem.Models
 {
+    public enum TipoSuite
+    {
+        Padrao = 1,
+        Luxo = 2,
+        Premium = 3
+    }
+
     public class Suite
     {
+
+        public TipoSuite TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+
         public Suite() { }
 
-        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+
+        public Suite(TipoSuite tipoSuite, int capacidade, decimal valorDiaria)
         {
             TipoSuite = tipoSuite;
             Capacidade = capacidade;
             ValorDiaria = valorDiaria;
         }
-
-        public string TipoSuite { get; set; }
-        public int Capacidade { get; set; }
-        public decimal ValorDiaria { get; set; }
     }
 }
